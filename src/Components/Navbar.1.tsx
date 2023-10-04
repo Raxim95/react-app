96824d67fb8b37fc7c7cc2761015e5cdaf7050b9
Raxim95 <raximongarbaev7@gmail.com>
2023-10-04 21:00:22 +0500

    commit3

--- src/Components/Navbar.1.tsx
+++ src/Components/Navbar.1.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
 
-export function Navbar() {
+export function MyNavbar() {
   return (
     <Navbar className="bg-dark">
       <Nav>
